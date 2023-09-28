5c4ea8032ae25ccc433351ce055e468dbd383f69
Bruno S Velho <bruno.velho@gmail.com>
2023-09-28 10:43:05 -0300

    Validando nome e e-mail

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError");
 
 const sqliteConnection = require("../database/sqlite");
@@ -32,7 +32,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email} = request.body;
+    const { name, email, password, old_password} = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
@@ -49,11 +49,30 @@ class UsersController {
       throw new AppError("Este e-mail já está sendo utilizado.");
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if(password && !old_password) {
+      throw new AppError("Você precisa informar a senha antiga para atualizar a senha.");
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if(!checkOldPassword) {
+        throw new AppError("Senha antiga incorreta.");
+      }
+
+      user.password = await hash(password, 8);
+    }
 
     await database.run(
-      "UPDATE users SET name = ?, email = ?, updated_at = ? WHERE id = ?",
+      `UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
       [name, email, new Date(), id]);
 
     response.json();
